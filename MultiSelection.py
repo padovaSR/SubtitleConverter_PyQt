dae9f68601ceae73a022f04056e6f312cdc8bfe3
padovaSR <g373en@gmail.com>
2023-02-06 04:44:44 +0100

    updated

--- MultiSelection.py
+++ MultiSelection.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
-################################################################################
 ## Form generated from reading UI file 'uiform2.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
+## Modified by padovaSR
 ##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
 
 from PySide2.QtCore import Qt, QSize
 from PySide2.QtGui import QIcon, QFont, QStandardItem, QStandardItemModel
