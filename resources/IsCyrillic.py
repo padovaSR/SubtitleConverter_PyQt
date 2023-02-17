45612f449feb3405976f7f3921f2a80a7d97107f
padovaSR <g373en@gmail.com>
2023-02-17 05:05:48 +0100

    updated

--- resources/IsCyrillic.py
+++ resources/IsCyrillic.py
@@ -7,13 +7,31 @@
 #-------------------------------------------------------------------------------
 
 import re
+from codecs import BOM_UTF8 
 
 import logging.config
 logger = logging.getLogger(__name__)
 
 
-def checkCyrillicAlphabet(input_text: str) -> bool:
+def checkCyrillicAlphabet(input_text):
     
+    def decode_text() -> str:
+        if input_text[:4].startswith(BOM_UTF8):
+            return input_text.decode("utf-8")
+        else:
+            for enc in ["cp1251", "utf-8"]:
+                try:
+                    input_text.decode(enc)
+                except:
+                    logger.debug(f"Error decoding with {enc}")
+                else:
+                    logger.debug(f"Decoding with: {enc}")
+                    break
+            return input_text.decode(enc)
+        
+    if isinstance(input_text, bytes):
+        input_text = decode_text()
+            
     def checkChars() -> int:
         def percentage(part, whole):
             try:
@@ -25,6 +43,7 @@ def checkCyrillicAlphabet(input_text: str) -> bool:
         st_pattern = re.compile(r"[\u0400-\u04FF]", re.U)
         rx = "".join((st_pattern.findall(input_text)))
         procenat = percentage(len(rx), len(list(filter(str.isalpha, input_text))))
+        logger.debug(f"Procenat ćirilice: {procenat} %")
         return procenat
 
     def maybeCyrillic() -> bool:
@@ -36,8 +55,7 @@ def checkCyrillicAlphabet(input_text: str) -> bool:
             return False
 
     if maybeCyrillic() is True:
-        if checkChars() > 60:
-            return True
-        else:
-            return False
+        return checkChars()
+    else:
+        return False
 
