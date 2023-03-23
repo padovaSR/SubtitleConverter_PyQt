7d94119d6f8f9cd3c582a6f95da991e261b52fc7
padovaSR <g373en@gmail.com>
2023-03-23 17:48:08 +0100

    updated

--- resources/renamer.py
+++ resources/renamer.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 
 class CollectFiles:
     """"""
-    EP = re.compile(r"epi(z|s)od(a|e)\s*-?\s*\W*\s*\d{,2}\.?|s\d{1,2}e\d{1,2}\.?|^\d{1,2}\.srt|\d{2}x\d{2}", (re.I|re.M))
+    EP = re.compile(r"epi(z|s)od(a|e)\s*-?\s*\W*\s*\d{,2}\.?|s\d{1,2}e\d{1,2}\.?|^\d{1,2}\.srt|\d{2}\s*x\s*\d{2}|s\d{1,2}\s*x\s*e\d{1,2}", (re.I|re.M))
     RP = re.compile(r"\d{4}\.?|(x|h)\.?26(4|5)|N(10|265)|5\.1\.?", re.I)
     subtitles = []
     def __init__(self, selected_folder=None):
