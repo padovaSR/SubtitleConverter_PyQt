bd0327848147b35d261e7c434af7a6b82359f4ba
padovaSR <g373en@gmail.com>
2024-09-10 17:27:37 +0200

    updated

--- resources/renamer.py
+++ resources/renamer.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 class CollectFiles:
     """"""
     EP = re.compile(r"epi(z|s)od(a|e)\s*-?\s*\W*\s*\d{,2}\.?|s\d{1,2}e\d{1,2}\.?|^\d{1,2}\.srt|\d{1,2}\s*x\s*\d{2}|s\d{1,2}\s*x\s*e\d{1,2}|\d{1,2}\.?\s?(ep)i?(z|s)?o?d?(a|e)?|^\w+ *\.*\d\d?", (re.I|re.M))
-    RP = re.compile(r"\d{4}\w?\.?|(x|h)\.?26(4|5)|N(10|265)|ddp5\.1\.?|\b\w{2,}\b(?<!\d)|[ \.-]|(ION\d{2,3})|(?<=part[.\- ])\d+|s\d+e|([a-z]+\.+){1,5}|\d+\.(?=s[0-9])", re.I)
+    RP = re.compile(r"\d{4}\w?\.?|(x|h)\.?26(4|5)|N(10|265)|ddp5\.1\.?|\b\w{2,}\b(?<!\d)|[ \.-]|(ION\d{2,3})|(?<=part[.\- ])\d+|s\d+e|([a-z]+\.+){1,5}|\d+\.(?=s[0-9])|(\([^\)]*\))|se(a|z)s*ona*\s*\d{1,2}", re.I)
     
     subtitles = []
     def __init__(self, selected_folder=None):
